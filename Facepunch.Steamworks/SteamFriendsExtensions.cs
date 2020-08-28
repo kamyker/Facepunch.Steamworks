9f40ad3b40b4f7a91ae9c969e55b6a9998deb856
kamyker <kamyker@o2.pl>
2020-08-28 12:57:10 +0200

    Extension to easily get players names that are not friends

---
+++ Facepunch.Steamworks/SteamFriendsExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Steamworks.Data;
+
+namespace Steamworks
+{
+	public static class SteamFriendsExtensions
+	{
+		/// <summary>
+		/// Works also for players that aren't friends
+		/// </summary>
+		public static void RequestName( this Friend player, Action<string> onRetrived )
+		{
+			if ( !SteamFriends.RequestUserInformation( player.Id, true ) )
+			{
+				onRetrived?.Invoke( player.Name );
+			}
+			else
+			{
+				var playerId = player.Id;
+				SteamFriends.OnPersonaStateChange += SteamFriends_OnPersonaStateChange;
+
+				void SteamFriends_OnPersonaStateChange( Friend friendChanged )
+				{
+					SteamFriends.OnPersonaStateChange -= SteamFriends_OnPersonaStateChange;
+					if ( playerId == friendChanged.Id )
+					{
+						onRetrived?.Invoke( friendChanged.Name );
+					}
+				}
+			}
+		}
+	}
+}

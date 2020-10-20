43cee721e66c9e1acd78c7f2c2ca9c02ccc8d49a
kamyker <kamyker@o2.pl>
2020-10-20 06:11:34 +0200

    ugc item get any preview image

---
+++ Facepunch.Steamworks/Structs/UgcItemExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Steamworks.Data;
+
+using QueryType = Steamworks.Ugc.Query;
+
+namespace Steamworks.Ugc
+{
+	public static class ItemExtensions
+	{
+		public static string GetPreviewImageOrAdditionalPreviewImageUrl(this Item item)
+		{
+			if ( !string.IsNullOrEmpty( item.PreviewImageUrl ) )
+				return item.PreviewImageUrl;
+			if ( item.AdditionalPreviews != null )
+			{
+				foreach ( var p in item.AdditionalPreviews )
+				{
+					if ( p.ItemPreviewType == ItemPreviewType.Image && !string.IsNullOrEmpty( p.UrlOrVideoID ) )
+						return p.UrlOrVideoID;
+				}
+			}
+			return null;
+		}
+
+	}
+}
